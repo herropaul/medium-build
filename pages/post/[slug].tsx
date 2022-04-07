f10146bf66be500dceb752a4efced288db4d8431
Paul Nguon <herropaul@gmail.com>
2022-04-07 16:41:09 -0700

    updated article design for posts

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -2,6 +2,7 @@ import {sanityClient, urlFor} from '../../sanity';
 import {GetStaticProps} from 'next';
 import Header from '../../components/Header';
 import {Post} from '../../typings';
+import PortableText from 'react-portable-text';
 
 interface Props {
     post: Post;
@@ -12,6 +13,42 @@ function Post ({post}: Props) {
     return(
         <main>
             <Header />
+            <img className='w-full h-40 object-cover'
+            src={urlFor(post.mainImage).url()!} alt=""/>
+
+            <article className=' max-w-3xl mx-auto p-5'>
+                <h1 className='text-3xl mt-10 mb-3'>{post.title}</h1>
+                <h2 className='text-xl font-light text-gray-500'>{post.description}</h2>
+
+                <div className='flex items-center space-x-3'>
+                    <img className='h-10 w-10 rounded-full'
+                    src={urlFor(post.author.image).url()!} alt=""/>
+                    <p className=' font-extralight text-sm'>Blog post by <span className=' text-green-600'>{post.author.name}</span>
+                    - Published at {new Date(post._createdAt).toLocaleString()} </p>
+                </div>
+
+                <div className='mt-10'>
+                    <PortableText 
+                    className=''
+                    dataset= {process.env.NEXT_PUBLIC_SANITY_DATASET}
+                    projectId= {process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
+                    content={post.body}
+                    serializers={{
+                        h1: (props: any) => (
+                            <h1 className='text-2xl font-bold my-5' {...props}/>
+                        ),
+                        h2: (props: any) => (
+                            <h2 className='text-xl font-bold my-5' {...props}/>
+                        ),
+                        li: ({children}: any) => (
+                            <li className='ml-5 list-disc'>{children}</li>
+                        ),
+                        link: ({children, href}: any) => (
+                            <a className='text-blue-600' href={href}>{children}</a>
+                        ),}}
+                    />
+                </div>
+            </article>
         </main>
     )
 }
@@ -43,7 +80,7 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps = async ({params}) => {
     const query =`
-    *[_type == 'post' && slug.current== 'why-satania-is-better-than-gabriel'][0]{
+    *[_type == 'post' && slug.current== $slug][0]{
         _id,
         _createdAt,
         title,
@@ -76,7 +113,8 @@ export const getStaticProps: GetStaticProps = async ({params}) => {
     return {
         props: {
             post,
-        }
+        },
+        revalidate: 60,
     }
 
 }
