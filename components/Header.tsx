bf346e5a3294f0d41aa9fb1e2797b038171e9909
Paul Nguon <herropaul@gmail.com>
2022-05-05 22:59:01 -0700

    added Pedium logos

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
+import Paul_Logo_Full from '../images/Paul_Logo_Full.png';
 
 function Header() {
   return (
     <header className='flex justify-between p-5 max-w-7xl mx-auto'> 
         <div className='flex items-center space-x-5'>
             <Link href="/">
-                <img className='w-44 object-contain cursor-pointer' src="https://links.papareact.com/yvf"/>
+                <img className='w-44 object-contain cursor-pointer' src="https://cdn.discordapp.com/attachments/972007130792075304/972010444032192573/Paul_Logo_Full.png"/>
             </Link>
             <div className='hidden md:inline-flex items-center space-x-5'>
                 <h3>About</h3>
--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ const Home = ({posts}: Props) => {
       <div className='flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0'>
         <div className='px-10 space-y-5'>
           <h1 className='text-6xl max-w-xl font-serif'>
-            <span className='underline decoration-black decoration-4'>Medium</span>{" "}
+            <span className='underline decoration-black decoration-4'>Pedium</span>{" "}
              is a place to write, read, and connect
           </h1>
           <h2>
@@ -32,7 +32,7 @@ const Home = ({posts}: Props) => {
           </h2>
         </div>
 
-      <img className='hidden md:inline-flex h-40 lg:h-full' src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" />
+      <img className='hidden md:inline-flex h-48 lg:h-full' src="https://cdn.discordapp.com/attachments/972007130792075304/972013948327063652/Paul_Logo_Letter.png" />
       </div>
 
       {/* Posts */}
