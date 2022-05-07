325adb161083e63c9b913c60fef56dc0808449c4
Paul Nguon <herropaul@gmail.com>
2022-05-07 14:19:51 -0700

    changed head title

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ const Home = ({posts}: Props) => {
   return (
     <div className=" max-w-7xl mx-auto">
       <Head>
-        <title>Medium Blog</title>
+        <title>Pedium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header/>
