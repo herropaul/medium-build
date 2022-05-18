07a611e7fccadefe1a66f23133e6a918a284a202
Paul Nguon <herropaul@gmail.com>
2022-05-17 22:37:36 -0700

    removed some headers

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,21 +5,11 @@ import Paul_Logo_Full from '../images/Paul_Logo_Full.png';
 
 function Header() {
   return (
-    <header className='flex justify-between p-5 max-w-7xl mx-auto'> 
+    <header className='flex justify-center p-5 max-w-7xl mx-auto'> 
         <div className='flex items-center space-x-5'>
             <Link href="/">
                 <img className='w-44 object-contain cursor-pointer' src="https://cdn.discordapp.com/attachments/972007130792075304/972010444032192573/Paul_Logo_Full.png"/>
             </Link>
-            <div className='hidden md:inline-flex items-center space-x-5'>
-                <h3>About</h3>
-                <h3>Contact</h3>
-                <h3 className='text-white bg-green-600 px-4 py-1 rounded-full'>Follow</h3>
-            </div>
-        </div>
-
-        <div className='flex items-center space-x-5 text-green-600'>
-             <h3>Sign In</h3>
-             <h3 className='border px-4 py-1 rounded-full border-green-500'>Get Started</h3>
         </div>
     </header>
   )
